3a070bb7e06531945628511c91917c96c15a072e
Radu Balan <radu.balan.97@gmail.com>
2018-04-02 12:51:21 +0300

    Add endpoint to get products by brand|category

--- backend/routes/public/product.route.js
+++ backend/routes/public/product.route.js
@@ -9,4 +9,20 @@ module.exports = (app) => {
       res.json({status: 404})
     }
   })
+  app.get('/api/products/:type/:name', async (req, res) => {
+    const productType = req.params.type
+    const typeName = req.params.name
+    try {
+      let products = []
+      if (productType === 'brand') products = await ProductService.getByBrand(typeName)
+      else if (productType === 'category') products = await ProductService.getByCategory(typeName)
+      else {
+        res.json({status: 404})
+        return
+      }
+      res.json({status: 200, data: products})
+    } catch (e) {
+      res.json({status: 404})
+    }
+  })
 }
